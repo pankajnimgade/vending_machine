23d2b7af56843f559326db4d50fa9050869d4498
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2017-12-26 21:24:54 -0500

    fixed layout for payment

--- app/src/main/java/com/nimgade/pk/vendingmachine/activities/user/interaction/view/UserInterfaceMainActivity.kt
+++ app/src/main/java/com/nimgade/pk/vendingmachine/activities/user/interaction/view/UserInterfaceMainActivity.kt
@@ -2,17 +2,29 @@ package com.nimgade.pk.vendingmachine.activities.user.interaction.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.nimgade.pk.vendingmachine.R
 import kotlinx.android.synthetic.main.activity_vending_machine_main.*
 
 class UserInterfaceMainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var makePayment: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vending_machine_main)
         setSupportActionBar(toolbar)
+        initializeUI()
+    }
+
+    private fun initializeUI() {
+        recyclerView = findViewById(R.id.UserInterfaceMainActivity_Product_List_RecyclerView)
+        makePayment = findViewById(R.id.UserInterfaceMainActivity_make_payment_button)
 
     }
 
